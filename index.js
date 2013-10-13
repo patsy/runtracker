09554398a391a5a351e05d5640ba614858d3c948
Patrik Norman <patrik.e.norman@gmail.com>
2013-10-13 21:47:11 +0200

    Added route that returns kittens stored in database

--- index.js
+++ index.js
@@ -4,44 +4,58 @@ var app = express();
 
 //Create database connection using mongoose
 var mongoose = require('mongoose');
-//the connectionstring means the mongodb running locally on your computer
-//under database runtracker using collection test
-//you will of course need to populate this to get out any data from the request
-var connectionString = 'mongodb://localhost/runtracker/test';
+//the connectionstring defines mongodb protocol, host and database
+var connectionString = 'mongodb://localhost/runtracker';
 mongoose.connect(connectionString);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error')); //bind errors to console
-//callback function is executed once the connection is open
-db.once('open', function callback () {
-	//do nothing so far
-});
+
 //define what a kitten looks like, it has a name and a sound
 var kittySchema = mongoose.Schema({
 		name: String,
 		sound: String
 	});
+
 //compile the kitten model accordning to schema
 var Kitten = mongoose.model('Kitten', kittySchema);
 
+//callback function that creates two kittens is executed once the connection is open
+db.once('open', function callback () {
+	if (Kitten.find() == null) {
+		var rolf = new Kitten({name: 'Rolf', sound: 'weeow'});
+		var max = new Kitten({name: 'Max', sound: 'piip'});
+		rolf.save(function(err){if (err) console.log('Error on save')});
+		max.save(function(err){if (err) console.log('Error on save')});
+	}
+	else {
+		console.log("Data already in collection")
+	}
+});
+
+//---------------------
+//Create routes
 app.listen(process.env.PORT || 5432);
 
 //Returns the sound of Rolf from mongodb
 app.get('/kitten', function(req, res){
- 	Kitten.find({name: 'Rolf'}, function (err, kittens) {
- 		if (err) {
- 			res.json("ERROR, ABONDON SHIP");
- 		}
- 		console.log(kittens);
- 		res.json(kittens);
- 	})
+	Kitten.find({name: 'Rolf'}, function (err, kittens) {
+		if (!err) {
+			res.json(kittens);
+		}
+		else {
+			res.json("ERROR, ABONDON SHIP");
+		}
+	})
 });
 
+//Fox route
 app.get('/fox', function(req, res){
   var q = "Hello my foxy pony!";
   res.json(q);
 });
 
+//Default route
 app.get('/', function(req, res){
   var q = "Hello my little pony!";
   res.json(q);
