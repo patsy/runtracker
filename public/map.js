354008eb27dbf1107846d543b8073b15af7eb4fa
Patrik Norman <patrik.e.norman@gmail.com>
2013-10-19 00:47:43 +0200

    Changed default zoom level

--- public/map.js
+++ public/map.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([57.664, 11.935], 13);
+var map = L.map('map').setView([57.664, 11.935], 11);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'})
